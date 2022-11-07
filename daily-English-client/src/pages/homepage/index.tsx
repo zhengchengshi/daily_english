ca360830eafde333660d0742e298cc27a5d376c8
zhengchengshi <2650744390@qq.com>
2022-11-08 00:52:35 +0800

    feat(DEC): homepage接口接入

--- daily-English-client/src/pages/homepage/index.tsx
+++ daily-English-client/src/pages/homepage/index.tsx
@@ -1,13 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import _ from "lodash";
 import { history } from "umi";
 import styles from "./index.less";
 import { getList, ListType } from "@/services";
 export default function index() {
   const [list, setList] = useState<ListType[]>();
+  const [filterItem, setFilterItem] = useState<ListType[]>();
+  const iptChange = _.debounce((e: InputEvent) => {
+    setFilterItem(
+      list?.filter(
+        (item) => item.name.indexOf((e.target as HTMLInputElement).value) !== -1
+      )
+    );
+  }, 1000);
   const fetchList = () => {
     getList().then((res) => {
-      console.log(res);
-      // setList(res);
+      setList(res);
+      setFilterItem(res);
     });
   };
   const emptyRender = () => {
@@ -28,15 +37,19 @@ export default function index() {
   return (
     <div className={styles.container}>
       <div className={styles.header}>Daily-English</div>
-      <input className={styles.searchBar} placeholder="搜索" />
+      <input
+        className={styles.searchBar}
+        placeholder="搜索"
+        onChange={iptChange}
+      />
       <div className={styles.itemContainer}>
-        {!!list?.length
-          ? list?.map((item, index) => (
+        {!!filterItem?.length
+          ? filterItem?.map((item, index) => (
               <div
                 key={item.sha}
                 className={styles.item}
                 onClick={() => {
-                  history.push("/detail");
+                  window.open(item.html_url);
                 }}
               >
                 {item.name || "-"}
